9ba8b0c4bb9fa8f1ed2ca3df99b2274f8add0392
Administrator <1476104805@qq.com>
2020-10-15 20:02:46 +0800

    添加：模拟多次随机散步

--- study/random/re_visual.py
+++ study/random/re_visual.py
@@ -3,13 +3,18 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-# 创建一个 RandomWalk 实例。
-rw = RandomWalk(100000)
-rw.fill_walk()
-# 将所有的点都绘制出来
-plt.style.use('classic')
-plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=1, c=(0, 0.8, 0))
-ax.set_title("漫步散点题", fontsize=20)
-plt.show()
+while True:
+    # 创建一个 RandomWalk 实例。
+    rw = RandomWalk(100000)
+    rw.fill_walk()
+    # 将所有的点都绘制出来
+    plt.style.use('classic')
+    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=1, c=(0, 0.8, 0))
+    ax.set_title("漫步散点题", fontsize=20)
+    plt.show()
+
+    keep_running = input("Make another walk?(y/n)")
+    if keep_running == 'n':
+        break
