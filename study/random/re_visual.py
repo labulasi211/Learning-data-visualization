e5e7254cfe7c14c31d2d380b2a5775f2577775ab
Administrator <1476104805@qq.com>
2020-10-15 22:26:47 +0800

    完成

--- study/random/re_visual.py
+++ study/random/re_visual.py
@@ -10,16 +10,12 @@ while True:
     # 将所有的点都绘制出来
     plt.style.use('classic')
     plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-    fig, ax = plt.subplots(figsize=(15, 9))
-    ax.scatter(rw.x_values, rw.y_values, s=1, c=range(rw.num_points), cmap=plt.cm.Blues, edgecolors='none')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
 
     # 突出起点和终点
     ax.scatter(0, 0, s=20, c='green', edgecolors='none')
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=20)
-
-    # 隐藏坐标轴
-    ax.get_yaxis().set_visible(False)
-    ax.get_xaxis().set_visible(False)
 
     ax.set_title("漫步散点图", fontsize=20)
     plt.show()
