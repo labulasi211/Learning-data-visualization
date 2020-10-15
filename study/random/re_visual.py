43735f26e1d8903d30ad6b4799f5f792f03886bf
Administrator <1476104805@qq.com>
2020-10-15 19:39:01 +0800

    添加：随机漫步目录（random）,实现制作漫步散点图

--- study/random/re_visual.py
+++ study/random/re_visual.py
@@ -4,11 +4,12 @@ from random_walk import RandomWalk
 
 
 # 创建一个 RandomWalk 实例。
-rw = RandomWalk()
+rw = RandomWalk(100000)
 rw.fill_walk()
 # 将所有的点都绘制出来
-plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 plt.style.use('classic')
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=10, c=(0, 0.8, 0))
+ax.scatter(rw.x_values, rw.y_values, s=1, c=(0, 0.8, 0))
+ax.set_title("漫步散点题", fontsize=20)
 plt.show()
