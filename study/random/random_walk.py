061439b07c3239957974e6e8cb2d8f570b71ba3b
Administrator <1476104805@qq.com>
2020-10-15 21:06:56 +0800

    绘制在y轴方向散步，观察图像。

--- study/random/random_walk.py
+++ study/random/random_walk.py
@@ -19,22 +19,13 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # 决定前进方向以及以及沿这个方向前进额距离
-            x_direction = choice([-1, 1])
-
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
             y_direction = choice([-1, 1])
 
-            y_distance = choice([0, 1, 2, 3, 4])
+            y_distance = choice(range(1, 11))
             y_step = y_distance * y_direction
 
-            # 拒绝原地踏步
-            if y_step == 0 or x_step == 0:
-                continue
-
             # 计算下一个点的x值和y值
-            x = self.x_values[-1] + x_step
+            x = self.x_values[-1] + 1
             y = self.y_values[-1] + y_step
 
             self.x_values.append(x)
