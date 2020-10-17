c8de0b699318dfa33db164c906a08089c305ce66
Administrator <1476104805@qq.com>
2020-10-17 16:36:30 +0800

    完成：练习15_8

--- study/dice/dice_visual.py
+++ study/dice/dice_visual.py
@@ -10,22 +10,22 @@ die_2 = Die(8)
 
 results = []
 for value in range(50_0000):
-    result = die_2.roll()+die_1.roll()
+    result = die_2.roll()*die_1.roll()
     results.append(result)
 
 # 分析结果
-max_result = die_2.num_sides + die_1.num_sides
+max_result = die_2.num_sides * die_1.num_sides
 frequencies = []
-for value in range(2, max_result+1):
+for value in range(1, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # 对结果进行可视化
-x_value = list(range(2, max_result+1))
+x_value = list(range(1, max_result+1))
 y_value = frequencies
 data = [Bar(x=x_value, y=y_value)]
 
 x_axis_config = {'title': '结果', 'dtick': 1}
 y_axis_config = {'title': '结果的频率'}
-my_layout = Layout(title='同时投一个D8 50000次的结果', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d8.html')
+my_layout = Layout(title='同时投两个D8面数相乘 50000次的结果', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d8 x d8.html')
