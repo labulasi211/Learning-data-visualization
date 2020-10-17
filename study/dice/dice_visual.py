543a3ab82aa8faa380ae1e1b8fa8d706982c1cd3
Administrator <1476104805@qq.com>
2020-10-17 16:29:08 +0800

    完成：练习15_6

--- study/dice/dice_visual.py
+++ study/dice/dice_visual.py
@@ -5,11 +5,11 @@ from plotly import offline
 from die import Die
 
 # 创建两个Die类
-die_1 = Die()
-die_2 = Die(10)
+die_1 = Die(8)
+die_2 = Die(8)
 
 results = []
-for value in range(50_000):
+for value in range(50_0000):
     result = die_2.roll()+die_1.roll()
     results.append(result)
 
@@ -27,5 +27,5 @@ data = [Bar(x=x_value, y=y_value)]
 
 x_axis_config = {'title': '结果', 'dtick': 1}
 y_axis_config = {'title': '结果的频率'}
-my_layout = Layout(title='同时投一个D6和一个D10 50000次的结果', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
+my_layout = Layout(title='同时投一个D8 50000次的结果', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d8.html')
