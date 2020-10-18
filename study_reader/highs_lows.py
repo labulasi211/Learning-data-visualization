fd38489a26f82191d49fabaf0da157ebbf41566b
Zeng <1476104805@qq.com>
2020-10-18 17:33:03 +0800

    读取文件：打印文件头以及其位置

--- study_reader/highs_lows.py
+++ study_reader/highs_lows.py
@@ -1,10 +1,17 @@
 import csv
+from datetime import datetime
 
 
 # 从文件中获取最高温度
-filename = 'data/sitka_weather_2018_full.csv'
+filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+
+    highs, dates = [], []
+    for row in reader:
+        dates.append(datetime.strptime(row[2], '%Y-%m-%d'))
+        highs.append(int(row[5]))
+
+    print(highs)
+    print(dates)
