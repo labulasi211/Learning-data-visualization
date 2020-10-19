a7abc9865bc967116e0ddc698a84ad36bb7e066a
Administrator <1476104805@qq.com>
2020-10-19 19:18:13 +0800

    添加：在图表中添加日期

--- study_reader/highs_lows.py
+++ study_reader/highs_lows.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 
 # 从文件中获取最高温度
@@ -13,5 +14,17 @@ with open(filename) as f:
         dates.append(datetime.strptime(row[2], '%Y-%m-%d'))
         highs.append(int(row[5]))
 
-    print(highs)
-    print(dates)
+# 根据最高温图绘制图形
+plt.style.use('seaborn')
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(dates, highs, c='red')
+
+# 设置图形的格式
+ax.set_title('2018年7月每日最高温度', fontsize=20)
+ax.set_xlabel('', fontsize=10)
+fig.autofmt_xdate()
+ax.set_ylabel('温度（F）', fontsize=10)
+ax.tick_params(axis='both', which='major', labelsize=10)
+
+plt.show()
