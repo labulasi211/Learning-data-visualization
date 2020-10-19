3d602a625ad6319675bffa17f3be515f21316028
Administrator <1476104805@qq.com>
2020-10-19 20:07:25 +0800

    添加：使用 try-except 来对csv文件进行错误检查

---
+++ study_reader/death_valley_highs_lows.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import matplotlib.pyplot as plt
+import csv
+
+
+file_name = 'data/death_valley_2018_simple.csv'
+with open(file_name) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # 从文件中获取日期，每日最高温度以及最低温度
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        dates.append(current_date)
+        try:
+            high = int(row[4])
+        except ValueError:
+            print(f'Missing data of high temperature for {current_date}.')
+        highs.append(high)
+        try:
+            low = int(row[5])
+        except ValueError:
+            print(f'Missing data of low temperature for {current_date}')
+        lows.append(low)
+
+# 根据每日最高温度与最低温度来绘制图形
+plt.style.use('seaborn')
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(dates, highs, c='red', linewidth=1, alpha=0.5)
+ax.plot(dates, lows, c='blue', linewidth=1, alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# 一些图像设置
+ax.set_title('2018死亡谷每日最高温度与最低温度', fontsize=20)
+ax.set_xlabel('日期', fontsize=10)
+ax.set_ylabel('温度（F）', fontsize=10)
+fig.autofmt_xdate()
+ax.tick_params(axis='both', which='major', labelsize=10)
+
+plt.show()
--- study_reader/highs_lows.py
+++ study_reader/highs_lows.py
@@ -3,25 +3,29 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-# 从文件中获取最高温度
-filename = 'data/sitka_weather_07-2018_simple.csv'
+# 从文件中获取每日最高温度，日期，以及每日最低温度
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs, dates = [], []
+    highs, dates, lows = [], [], []
     for row in reader:
         dates.append(datetime.strptime(row[2], '%Y-%m-%d'))
         highs.append(int(row[5]))
+        lows.append(int(row[6]))
 
-# 根据最高温图绘制图形
+
+# 根据最高温和最低温图绘制图形
 plt.style.use('seaborn')
 plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 fig, ax = plt.subplots(figsize=(10, 6))
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', linewidth=1, alpha=0.5)
+ax.plot(dates, lows, c='blue', linewidth=1, alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # 设置图形的格式
-ax.set_title('2018年7月每日最高温度', fontsize=20)
+ax.set_title('2018年每日最高温度与最低温度', fontsize=20)
 ax.set_xlabel('', fontsize=10)
 fig.autofmt_xdate()
 ax.set_ylabel('温度（F）', fontsize=10)
